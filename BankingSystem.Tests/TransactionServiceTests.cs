b76e318b4b5b590d5a4c2b9255412a6cc06b232f
lavalishvili <lavalishvili@LHO10048790>
2023-04-03 00:27:27 +0400

    add withdrawService Test

--- BankingSystem.Tests/TransactionServiceTests.cs
+++ BankingSystem.Tests/TransactionServiceTests.cs
@@ -8,6 +8,7 @@ using BankingSystem.DB;
 using BankingSystem.Features.Reports;
 using Microsoft.EntityFrameworkCore;
 using BankingSystem.Features.InternetBank.User.Transactions;
+using BankingSystem.Features.ATM.Withdraw;
 
 namespace BankingSystem.Tests
 {
@@ -33,13 +34,53 @@ namespace BankingSystem.Tests
             db.Accounts.AddRange(accounts);
             await db.SaveChangesAsync();
             var transactionRepository = new TransactionRepository(db);
-            var convertService = new ConvertService();
+            var convertService = new ConvertService(transactionRepository);
             var transactionService = new TransactionService(convertService, transactionRepository);
             var result = await transactionService.TransferFunds(transactionRequest, "1");
             var accountsbalance = accounts[0].Balance;
 
-
+            
             Assert.That(accounts[0].Balance, Is.EqualTo(98.5));
+            Assert.That(accounts[1].Balance, Is.EqualTo(100));
+
+
+        }
+
+        [Test]
+        public async Task WithdrawService77Test()
+        {
+            WithdrawRequest withdrawRequest = new WithdrawRequest()
+            {
+                Amount = 100,
+                CardNumber = "1111111111111111",
+                Currency = Currency.GEL,
+                PIN = "1234"
+            };
+
+            List<AccountEntity> accounts = new List<AccountEntity>()
+            {
+                new AccountEntity() { Balance = 1000, Currency= 0, IBAN="1", Id=1, UserId= 1},
+
+            };
+
+            var card = new CardEntity { CardNumber = "1111111111111111", PIN = "1234", CVV = "111", AccountId = 1, ExpirationDate = DateTime.UtcNow.AddDays(1) };
+
+
+            using var db = new AppDbContext(GetDbContextOptions());
+            db.Database.EnsureCreated();
+            db.Accounts.AddRange(accounts);
+            db.Cards.Add(card);
+            await db.SaveChangesAsync();
+            var WithdrawRepository = new WithdrawRepository(db);
+            var transacitonRepository = new TransactionRepository(db); // 
+            var convertService = new ConvertService(transacitonRepository); //i dont know what's the use of this
+            var withdrawService = new WithdrawService(convertService, WithdrawRepository);
+            var result = await withdrawService.WithdawFromAtm(withdrawRequest);
+            var accountsbalance = accounts[0].Balance;
+
+
+            Assert.That(accounts[0].Balance, Is.EqualTo(898));
+
         }
 
         private DbContextOptions<AppDbContext> GetDbContextOptions()
