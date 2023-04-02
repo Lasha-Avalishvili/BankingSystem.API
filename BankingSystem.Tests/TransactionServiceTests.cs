67e982ed041d6c33fd280a579245d657cf92e0df
lavalishvili <lavalishvili@LHO10048790>
2023-04-03 00:28:51 +0400

    delete unnecessary part

--- BankingSystem.Tests/TransactionServiceTests.cs
+++ BankingSystem.Tests/TransactionServiceTests.cs
@@ -44,44 +44,7 @@ namespace BankingSystem.Tests
             Assert.That(accounts[1].Balance, Is.EqualTo(100));
 
 
-        }
-
-        [Test]
-        public async Task WithdrawService77Test()
-        {
-            WithdrawRequest withdrawRequest = new WithdrawRequest()
-            {
-                Amount = 100,
-                CardNumber = "1111111111111111",
-                Currency = Currency.GEL,
-                PIN = "1234"
-            };
-
-            List<AccountEntity> accounts = new List<AccountEntity>()
-            {
-                new AccountEntity() { Balance = 1000, Currency= 0, IBAN="1", Id=1, UserId= 1},
-
-            };
-
-            var card = new CardEntity { CardNumber = "1111111111111111", PIN = "1234", CVV = "111", AccountId = 1, ExpirationDate = DateTime.UtcNow.AddDays(1) };
-
-
-            using var db = new AppDbContext(GetDbContextOptions());
-            db.Database.EnsureCreated();
-            db.Accounts.AddRange(accounts);
-            db.Cards.Add(card);
-            await db.SaveChangesAsync();
-            var WithdrawRepository = new WithdrawRepository(db);
-            var transacitonRepository = new TransactionRepository(db); // 
-            var convertService = new ConvertService(transacitonRepository); //i dont know what's the use of this
-            var withdrawService = new WithdrawService(convertService, WithdrawRepository);
-            var result = await withdrawService.WithdawFromAtm(withdrawRequest);
-            var accountsbalance = accounts[0].Balance;
-
-
-            Assert.That(accounts[0].Balance, Is.EqualTo(898));
-
-        }
+        } 
 
         private DbContextOptions<AppDbContext> GetDbContextOptions()
         {
