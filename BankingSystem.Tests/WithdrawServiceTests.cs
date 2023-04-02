de478cc5f07641bbebab9e9ad03abaa211bfaded
lavalishvili <lavalishvili@LHO10048790>
2023-04-03 00:31:07 +0400

    uncomment code

--- BankingSystem.Tests/WithdrawServiceTests.cs
+++ BankingSystem.Tests/WithdrawServiceTests.cs
@@ -1,53 +1,56 @@
-﻿//using BankingSystem.DB.Entities;
-//using BankingSystem.DB;
-//using BankingSystem.Features.InternetBank.User.Transactions;
-//using Microsoft.EntityFrameworkCore;
-//using BankingSystem.Features.ATM.Withdraw;
-//using System.Runtime.Intrinsics.X86;
+﻿using BankingSystem.DB.Entities;
+using BankingSystem.DB;
+using BankingSystem.Features.InternetBank.User.Transactions;
+using Microsoft.EntityFrameworkCore;
+using BankingSystem.Features.ATM.Withdraw;
+using System.Runtime.Intrinsics.X86;
 
-//namespace BankingSystem.Tests
-//{
-//    public class WithdrawServiceTests
-//    {
-//        [Test]
-//        public async Task WithdrawServiceTest()
-//        {
-//            WithdrawRequest withdrawRequest = new WithdrawRequest()
-//            {
-//                Amount= 100, CardNumber="1111111111111111", Currency= Currency.GEL, PIN="1234"
-//            };
+namespace BankingSystem.Tests
+{
+    public class WithdrawServiceTests
+    {
+        [Test]
+        public async Task WithdrawServiceTest()
+        {
+            WithdrawRequest withdrawRequest = new WithdrawRequest()
+            {
+                Amount = 100,
+                CardNumber = "1111111111111111",
+                Currency = Currency.GEL,
+                PIN = "1234"
+            };
 
-//            List<AccountEntity> accounts = new List<AccountEntity>()
-//            {
-//                new AccountEntity() { Balance = 1000, Currency= 0, IBAN="1", Id=1, UserId= 1},
-               
-//            };
+            List<AccountEntity> accounts = new List<AccountEntity>()
+            {
+                new AccountEntity() { Balance = 1000, Currency= 0, IBAN="1", Id=1, UserId= 1},
 
-//            var card = new CardEntity { CardNumber = "1111111111111111", PIN = "1234", CVV="111", AccountId = 1, ExpirationDate = DateTime.UtcNow.AddDays(1) };
+            };
 
-
-//            using var db = new AppDbContext(GetDbContextOptions());
-//            db.Database.EnsureCreated();
-//            db.Accounts.AddRange(accounts);
-//            db.Cards.Add(card);
-//            await db.SaveChangesAsync();
-//            var WithdrawRepository = new WithdrawRepository(db);
-//            var transacitonRepository = new TransactionRepository(db); // 
-//            var convertService = new ConvertService(transacitonRepository); //i dont know what's the use of this
-//            var withdrawService = new WithdrawService(convertService, WithdrawRepository);
-//            var result = await withdrawService.WithdawFromAtm(withdrawRequest);
-//            var accountsbalance = accounts[0].Balance;
+            var card = new CardEntity { CardNumber = "1111111111111111", PIN = "1234", CVV = "111", AccountId = 1, ExpirationDate = DateTime.UtcNow.AddDays(1) };
 
 
-//            Assert.That(accounts[0].Balance, Is.EqualTo(898));
+            using var db = new AppDbContext(GetDbContextOptions());
+            db.Database.EnsureCreated();
+            db.Accounts.AddRange(accounts);
+            db.Cards.Add(card);
+            await db.SaveChangesAsync();
+            var WithdrawRepository = new WithdrawRepository(db);
+            var transacitonRepository = new TransactionRepository(db); // 
+            var convertService = new ConvertService(transacitonRepository); //i dont know what's the use of this
+            var withdrawService = new WithdrawService(convertService, WithdrawRepository);
+            var result = await withdrawService.WithdawFromAtm(withdrawRequest);
+            var accountsbalance = accounts[0].Balance;
 
-//        }
 
-//        private DbContextOptions<AppDbContext> GetDbContextOptions()
-//        {
-//            var builder = new DbContextOptionsBuilder<AppDbContext>();
-//            builder.UseInMemoryDatabase("inmemorydb");
-//            return builder.Options;
-//        }
-//    }
-//}
+            Assert.That(accounts[0].Balance, Is.EqualTo(898));
+
+        }
+
+        private DbContextOptions<AppDbContext> GetDbContextOptions()
+        {
+            var builder = new DbContextOptionsBuilder<AppDbContext>();
+            builder.UseInMemoryDatabase("inmemorydb");
+            return builder.Options;
+        }
+    }
+}
